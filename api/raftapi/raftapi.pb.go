87c9079b9ff87474e26b11acdd234c05865ff058
nwillc <nwillc@gmail.com>
2020-12-28 11:48:27 -0500

    Protoc upddates

--- api/raftapi/raftapi.pb.go
+++ api/raftapi/raftapi.pb.go
@@ -2,16 +2,12 @@
 // versions:
 // 	protoc-gen-go v1.25.0
 // 	protoc        v3.14.0
-// source: raftapi.proto
+// source: api/raftapi/raftapi.proto
 
 package raftapi
 
 import (
-	context "context"
 	proto "github.com/golang/protobuf/proto"
-	grpc "google.golang.org/grpc"
-	codes "google.golang.org/grpc/codes"
-	status "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
@@ -38,7 +34,7 @@ type Empty struct {
 func (x *Empty) Reset() {
 	*x = Empty{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_raftapi_proto_msgTypes[0]
+		mi := &file_api_raftapi_raftapi_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -51,7 +47,7 @@ func (x *Empty) String() string {
 func (*Empty) ProtoMessage() {}
 
 func (x *Empty) ProtoReflect() protoreflect.Message {
-	mi := &file_raftapi_proto_msgTypes[0]
+	mi := &file_api_raftapi_raftapi_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -64,7 +60,62 @@ func (x *Empty) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Empty.ProtoReflect.Descriptor instead.
 func (*Empty) Descriptor() ([]byte, []int) {
-	return file_raftapi_proto_rawDescGZIP(), []int{0}
+	return file_api_raftapi_raftapi_proto_rawDescGZIP(), []int{0}
+}
+
+type WhoAmI struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Name string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	Port uint32 `protobuf:"varint,2,opt,name=port,proto3" json:"port,omitempty"`
+}
+
+func (x *WhoAmI) Reset() {
+	*x = WhoAmI{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_api_raftapi_raftapi_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *WhoAmI) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*WhoAmI) ProtoMessage() {}
+
+func (x *WhoAmI) ProtoReflect() protoreflect.Message {
+	mi := &file_api_raftapi_raftapi_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use WhoAmI.ProtoReflect.Descriptor instead.
+func (*WhoAmI) Descriptor() ([]byte, []int) {
+	return file_api_raftapi_raftapi_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *WhoAmI) GetName() string {
+	if x != nil {
+		return x.Name
+	}
+	return ""
+}
+
+func (x *WhoAmI) GetPort() uint32 {
+	if x != nil {
+		return x.Port
+	}
+	return 0
 }
 
 type Bool struct {
@@ -78,7 +129,7 @@ type Bool struct {
 func (x *Bool) Reset() {
 	*x = Bool{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_raftapi_proto_msgTypes[1]
+		mi := &file_api_raftapi_raftapi_proto_msgTypes[2]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -91,7 +142,7 @@ func (x *Bool) String() string {
 func (*Bool) ProtoMessage() {}
 
 func (x *Bool) ProtoReflect() protoreflect.Message {
-	mi := &file_raftapi_proto_msgTypes[1]
+	mi := &file_api_raftapi_raftapi_proto_msgTypes[2]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -104,7 +155,7 @@ func (x *Bool) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Bool.ProtoReflect.Descriptor instead.
 func (*Bool) Descriptor() ([]byte, []int) {
-	return file_raftapi_proto_rawDescGZIP(), []int{1}
+	return file_api_raftapi_raftapi_proto_rawDescGZIP(), []int{2}
 }
 
 func (x *Bool) GetStatus() bool {
@@ -123,7 +174,7 @@ type RequestVoteRequest struct {
 func (x *RequestVoteRequest) Reset() {
 	*x = RequestVoteRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_raftapi_proto_msgTypes[2]
+		mi := &file_api_raftapi_raftapi_proto_msgTypes[3]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -136,7 +187,7 @@ func (x *RequestVoteRequest) String() string {
 func (*RequestVoteRequest) ProtoMessage() {}
 
 func (x *RequestVoteRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_raftapi_proto_msgTypes[2]
+	mi := &file_api_raftapi_raftapi_proto_msgTypes[3]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -149,21 +200,25 @@ func (x *RequestVoteRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use RequestVoteRequest.ProtoReflect.Descriptor instead.
 func (*RequestVoteRequest) Descriptor() ([]byte, []int) {
-	return file_raftapi_proto_rawDescGZIP(), []int{2}
+	return file_api_raftapi_raftapi_proto_rawDescGZIP(), []int{3}
 }
 
-var File_raftapi_proto protoreflect.FileDescriptor
+var File_api_raftapi_raftapi_proto protoreflect.FileDescriptor
 
-var file_raftapi_proto_rawDesc = []byte{
-	0x0a, 0x0d, 0x72, 0x61, 0x66, 0x74, 0x61, 0x70, 0x69, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12,
-	0x07, 0x72, 0x61, 0x66, 0x74, 0x61, 0x70, 0x69, 0x22, 0x07, 0x0a, 0x05, 0x45, 0x6d, 0x70, 0x74,
-	0x79, 0x22, 0x1e, 0x0a, 0x04, 0x42, 0x6f, 0x6f, 0x6c, 0x12, 0x16, 0x0a, 0x06, 0x73, 0x74, 0x61,
-	0x74, 0x75, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x08, 0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75,
-	0x73, 0x22, 0x14, 0x0a, 0x12, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x56, 0x6f, 0x74, 0x65,
-	0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x32, 0x73, 0x0a, 0x0b, 0x52, 0x61, 0x66, 0x74, 0x53,
-	0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x27, 0x0a, 0x04, 0x50, 0x69, 0x6e, 0x67, 0x12, 0x0e,
-	0x2e, 0x72, 0x61, 0x66, 0x74, 0x61, 0x70, 0x69, 0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x1a, 0x0d,
-	0x2e, 0x72, 0x61, 0x66, 0x74, 0x61, 0x70, 0x69, 0x2e, 0x42, 0x6f, 0x6f, 0x6c, 0x22, 0x00, 0x12,
+var file_api_raftapi_raftapi_proto_rawDesc = []byte{
+	0x0a, 0x19, 0x61, 0x70, 0x69, 0x2f, 0x72, 0x61, 0x66, 0x74, 0x61, 0x70, 0x69, 0x2f, 0x72, 0x61,
+	0x66, 0x74, 0x61, 0x70, 0x69, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x07, 0x72, 0x61, 0x66,
+	0x74, 0x61, 0x70, 0x69, 0x22, 0x07, 0x0a, 0x05, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x22, 0x30, 0x0a,
+	0x06, 0x57, 0x68, 0x6f, 0x41, 0x6d, 0x49, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18,
+	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x70,
+	0x6f, 0x72, 0x74, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0d, 0x52, 0x04, 0x70, 0x6f, 0x72, 0x74, 0x22,
+	0x1e, 0x0a, 0x04, 0x42, 0x6f, 0x6f, 0x6c, 0x12, 0x16, 0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75,
+	0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x08, 0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x22,
+	0x14, 0x0a, 0x12, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x56, 0x6f, 0x74, 0x65, 0x52, 0x65,
+	0x71, 0x75, 0x65, 0x73, 0x74, 0x32, 0x75, 0x0a, 0x0b, 0x52, 0x61, 0x66, 0x74, 0x53, 0x65, 0x72,
+	0x76, 0x69, 0x63, 0x65, 0x12, 0x29, 0x0a, 0x04, 0x50, 0x69, 0x6e, 0x67, 0x12, 0x0e, 0x2e, 0x72,
+	0x61, 0x66, 0x74, 0x61, 0x70, 0x69, 0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x1a, 0x0f, 0x2e, 0x72,
+	0x61, 0x66, 0x74, 0x61, 0x70, 0x69, 0x2e, 0x57, 0x68, 0x6f, 0x41, 0x6d, 0x49, 0x22, 0x00, 0x12,
 	0x3b, 0x0a, 0x0b, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x56, 0x6f, 0x74, 0x65, 0x12, 0x1b,
 	0x2e, 0x72, 0x61, 0x66, 0x74, 0x61, 0x70, 0x69, 0x2e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74,
 	0x56, 0x6f, 0x74, 0x65, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x0d, 0x2e, 0x72, 0x61,
@@ -173,28 +228,29 @@ var file_raftapi_proto_rawDesc = []byte{
 }
 
 var (
-	file_raftapi_proto_rawDescOnce sync.Once
-	file_raftapi_proto_rawDescData = file_raftapi_proto_rawDesc
+	file_api_raftapi_raftapi_proto_rawDescOnce sync.Once
+	file_api_raftapi_raftapi_proto_rawDescData = file_api_raftapi_raftapi_proto_rawDesc
 )
 
-func file_raftapi_proto_rawDescGZIP() []byte {
-	file_raftapi_proto_rawDescOnce.Do(func() {
-		file_raftapi_proto_rawDescData = protoimpl.X.CompressGZIP(file_raftapi_proto_rawDescData)
+func file_api_raftapi_raftapi_proto_rawDescGZIP() []byte {
+	file_api_raftapi_raftapi_proto_rawDescOnce.Do(func() {
+		file_api_raftapi_raftapi_proto_rawDescData = protoimpl.X.CompressGZIP(file_api_raftapi_raftapi_proto_rawDescData)
 	})
-	return file_raftapi_proto_rawDescData
+	return file_api_raftapi_raftapi_proto_rawDescData
 }
 
-var file_raftapi_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
-var file_raftapi_proto_goTypes = []interface{}{
+var file_api_raftapi_raftapi_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
+var file_api_raftapi_raftapi_proto_goTypes = []interface{}{
 	(*Empty)(nil),              // 0: raftapi.Empty
-	(*Bool)(nil),               // 1: raftapi.Bool
-	(*RequestVoteRequest)(nil), // 2: raftapi.RequestVoteRequest
+	(*WhoAmI)(nil),             // 1: raftapi.WhoAmI
+	(*Bool)(nil),               // 2: raftapi.Bool
+	(*RequestVoteRequest)(nil), // 3: raftapi.RequestVoteRequest
 }
-var file_raftapi_proto_depIdxs = []int32{
+var file_api_raftapi_raftapi_proto_depIdxs = []int32{
 	0, // 0: raftapi.RaftService.Ping:input_type -> raftapi.Empty
-	2, // 1: raftapi.RaftService.RequestVote:input_type -> raftapi.RequestVoteRequest
-	1, // 2: raftapi.RaftService.Ping:output_type -> raftapi.Bool
-	1, // 3: raftapi.RaftService.RequestVote:output_type -> raftapi.Bool
+	3, // 1: raftapi.RaftService.RequestVote:input_type -> raftapi.RequestVoteRequest
+	1, // 2: raftapi.RaftService.Ping:output_type -> raftapi.WhoAmI
+	2, // 3: raftapi.RaftService.RequestVote:output_type -> raftapi.Bool
 	2, // [2:4] is the sub-list for method output_type
 	0, // [0:2] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -202,13 +258,13 @@ var file_raftapi_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_raftapi_proto_init() }
-func file_raftapi_proto_init() {
-	if File_raftapi_proto != nil {
+func init() { file_api_raftapi_raftapi_proto_init() }
+func file_api_raftapi_raftapi_proto_init() {
+	if File_api_raftapi_raftapi_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_raftapi_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_api_raftapi_raftapi_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Empty); i {
 			case 0:
 				return &v.state
@@ -220,7 +276,19 @@ func file_raftapi_proto_init() {
 				return nil
 			}
 		}
-		file_raftapi_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_api_raftapi_raftapi_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*WhoAmI); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_api_raftapi_raftapi_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Bool); i {
 			case 0:
 				return &v.state
@@ -232,7 +300,7 @@ func file_raftapi_proto_init() {
 				return nil
 			}
 		}
-		file_raftapi_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+		file_api_raftapi_raftapi_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*RequestVoteRequest); i {
 			case 0:
 				return &v.state
@@ -249,134 +317,18 @@ func file_raftapi_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_raftapi_proto_rawDesc,
+			RawDescriptor: file_api_raftapi_raftapi_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   3,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_raftapi_proto_goTypes,
-		DependencyIndexes: file_raftapi_proto_depIdxs,
-		MessageInfos:      file_raftapi_proto_msgTypes,
+		GoTypes:           file_api_raftapi_raftapi_proto_goTypes,
+		DependencyIndexes: file_api_raftapi_raftapi_proto_depIdxs,
+		MessageInfos:      file_api_raftapi_raftapi_proto_msgTypes,
 	}.Build()
-	File_raftapi_proto = out.File
-	file_raftapi_proto_rawDesc = nil
-	file_raftapi_proto_goTypes = nil
-	file_raftapi_proto_depIdxs = nil
-}
-
-// Reference imports to suppress errors if they are not otherwise used.
-var _ context.Context
-var _ grpc.ClientConnInterface
-
-// This is a compile-time assertion to ensure that this generated file
-// is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion6
-
-// RaftServiceClient is the client API for RaftService service.
-//
-// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type RaftServiceClient interface {
-	Ping(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*Bool, error)
-	RequestVote(ctx context.Context, in *RequestVoteRequest, opts ...grpc.CallOption) (*Bool, error)
-}
-
-type raftServiceClient struct {
-	cc grpc.ClientConnInterface
-}
-
-func NewRaftServiceClient(cc grpc.ClientConnInterface) RaftServiceClient {
-	return &raftServiceClient{cc}
-}
-
-func (c *raftServiceClient) Ping(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*Bool, error) {
-	out := new(Bool)
-	err := c.cc.Invoke(ctx, "/raftapi.RaftService/Ping", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *raftServiceClient) RequestVote(ctx context.Context, in *RequestVoteRequest, opts ...grpc.CallOption) (*Bool, error) {
-	out := new(Bool)
-	err := c.cc.Invoke(ctx, "/raftapi.RaftService/RequestVote", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-// RaftServiceServer is the server API for RaftService service.
-type RaftServiceServer interface {
-	Ping(context.Context, *Empty) (*Bool, error)
-	RequestVote(context.Context, *RequestVoteRequest) (*Bool, error)
-}
-
-// UnimplementedRaftServiceServer can be embedded to have forward compatible implementations.
-type UnimplementedRaftServiceServer struct {
-}
-
-func (*UnimplementedRaftServiceServer) Ping(context.Context, *Empty) (*Bool, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Ping not implemented")
-}
-func (*UnimplementedRaftServiceServer) RequestVote(context.Context, *RequestVoteRequest) (*Bool, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method RequestVote not implemented")
-}
-
-func RegisterRaftServiceServer(s *grpc.Server, srv RaftServiceServer) {
-	s.RegisterService(&_RaftService_serviceDesc, srv)
-}
-
-func _RaftService_Ping_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Empty)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(RaftServiceServer).Ping(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/raftapi.RaftService/Ping",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(RaftServiceServer).Ping(ctx, req.(*Empty))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _RaftService_RequestVote_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(RequestVoteRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(RaftServiceServer).RequestVote(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/raftapi.RaftService/RequestVote",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(RaftServiceServer).RequestVote(ctx, req.(*RequestVoteRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-var _RaftService_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "raftapi.RaftService",
-	HandlerType: (*RaftServiceServer)(nil),
-	Methods: []grpc.MethodDesc{
-		{
-			MethodName: "Ping",
-			Handler:    _RaftService_Ping_Handler,
-		},
-		{
-			MethodName: "RequestVote",
-			Handler:    _RaftService_RequestVote_Handler,
-		},
-	},
-	Streams:  []grpc.StreamDesc{},
-	Metadata: "raftapi.proto",
+	File_api_raftapi_raftapi_proto = out.File
+	file_api_raftapi_raftapi_proto_rawDesc = nil
+	file_api_raftapi_raftapi_proto_goTypes = nil
+	file_api_raftapi_raftapi_proto_depIdxs = nil
 }
--- bin/grpc.sh
+++ bin/grpc.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+go get -u google.golang.org/protobuf/cmd/protoc-gen-go \
+    google.golang.org/grpc/cmd/protoc-gen-go-grpc
 
-protoc -I api/raftapi/ api/raftapi/raftapi.proto --go_out=plugins=grpc:api/raftapi/
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    api/raftapi/raftapi.proto
