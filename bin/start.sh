6c995b4a07774dc0f6299fd99e4d4137a14cf79a
nwillc <nwillc@gmail.com>
2021-01-09 20:51:29 -0500

    reorganizing files

--- bin/start.sh
+++ bin/start.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd -P)"
 
 set -eou pipefail
 
-(cd pkg ; go build)
+cd "${SCRIPT_DIR}/../pkg"
+
+go build
 
 for member in one two three four five; do
-  ./pkg/goraft -member ${member} &
+  ./goraft -member ${member} &
 done
